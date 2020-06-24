6f5a355060f3c93dca07c154d2daa31562e3eec4
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 11:02:21 +0300

    move alphabet to method

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 def caesar_cipher(string, key)
+  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   answer = []
   string_array = string.split('')
   string_array.each do | letter |
