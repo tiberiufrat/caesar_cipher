886b4c5701ff5a4a9c795f9cbb22a1bf9c479d2e
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 10:59:30 +0300

    add method caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,14 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar_cipher(string, key)
-  
+  answer = []
+  string_array = string.split('')
+  string_array.each do | letter |
+    if alphabet.include?(letter)
+      current_position = alphabet.index(letter)
+      new_position = (current_position + key) % 27
+      new_letter = alphabet[new_position]
+      answer.push(new_letter)
+    end
+  end
 end
