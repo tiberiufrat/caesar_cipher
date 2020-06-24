b977798e79f8f6fc83ac6a2312965710bc6e41d6
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 11:12:16 +0300

    turn array to range

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,5 @@
 def caesar_cipher(string, key)
-  lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  lowercase = ('a'..'z').to_a
   uppercase = lowercase.map(&:upcase)
   answer = []
   string_array = string.split('')
