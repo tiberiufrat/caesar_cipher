f754a6f5297bf15d41230f43fba6bbc3dcfe98f7
tiberiufrat <tiberiu.fratila@gmail.com>
2020-06-24 11:08:20 +0300

    add support for uppercase

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,18 +1,24 @@
 def caesar_cipher(string, key)
-  alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  uppercase = lowercase.map(&:upcase)
   answer = []
   string_array = string.split('')
   string_array.each do | letter |
-    if alphabet.include?(letter)
-      current_position = alphabet.index(letter)
-      new_position = (current_position + key.to_i) % 27
-      new_letter = alphabet[new_position]
+    if lowercase.include?(letter)
+      current_position = lowercase.index(letter)
+      new_position = (current_position + key.to_i) % 26
+      new_letter = lowercase[new_position]
+      answer.push(new_letter)
+    elsif uppercase.include?(letter)
+      current_position = uppercase.index(letter)
+      new_position = (current_position + key.to_i) % 26
+      new_letter = uppercase[new_position]
       answer.push(new_letter)
     else
       answer.push(letter)
     end
   end
-  answer
+  answer.join
 end
 
-puts caesar_cipher('What a string!', 5)
+p caesar_cipher('What a string!', 5)
